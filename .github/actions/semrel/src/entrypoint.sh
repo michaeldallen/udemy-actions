070b33f4a7d3c3667812fb7cfbe45ae5267db21e
michaeldallen <michael@allenhub.net>
2023-11-28 15:39:12 -0500

    Update entrypoint.sh

--- .github/actions/semrel/src/entrypoint.sh
+++ .github/actions/semrel/src/entrypoint.sh
@@ -19,7 +19,7 @@ set 2>&1 | sed 's/^/set:/'
 # if the permissions are correct we should be able to create and delete a tag on origin
 git tag -a -m aaa foo
 git push origin foo
-git push -d origin foo
-git tag -d foo
+#git push -d origin foo
+#git tag -d foo
 
 npx --no-install semantic-release --dry-run
