1811d3002d38af04394cb8d202b25e521241842f
Michael Allen <mallen@mathworks.com>
2023-11-28 15:54:38 -0500

    updates from work reconciliaiton

--- .github/actions/semrel/src/entrypoint.sh
+++ .github/actions/semrel/src/entrypoint.sh
@@ -10,18 +10,13 @@ git config --global user.name "Your Name"
   
 pwd| sed 's/^/pwd:/'
 
-
+echo $(pwd) | sed 's/^/pwd:/'
 find . | fgrep -v .git | sort | sed 's/^/find:/'
 
 
 set 2>&1 | sed 's/^/set:/'
 
 
-# if the permissions are correct we should be able to create and delete a tag on origin
-git tag -a -m aaa foo
-git push origin foo
-git push -d origin foo
-git tag -d foo
 
 
 cp -v /workdir/src/dot_releaserc .releaserc 
